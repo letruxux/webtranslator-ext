cb4bddb41b6335ecaad503be46730ba661bc73ba
letruxux <102257756+letruxux@users.noreply.github.com>
2023-01-06 21:49:13 +0100

    Update background.js

--- source/background.js
+++ source/background.js
@@ -1,6 +1,1 @@
-chrome.action.onClicked.addListener(function (tab) {
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        var res = "https://translate.google.com/translate?sl=auto&tl=" + navigator.language + "&u=" + tabs[0].url
-        chrome.tabs.create({ url: res });
-    });
-});
+chrome.action.onClicked.addListener(function (tab) {chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {chrome.tabs.create({ url: "https://translate.google.com/translate?sl=auto&tl=" + navigator.language + "&u=" + tabs[0].url }); }); });
