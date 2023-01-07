3b7f2c2ed74e723f19b466b45c7f3bc88d2a677f
letruxux <102257756+letruxux@users.noreply.github.com>
2023-01-07 23:50:07 +0100

    Update background.js

--- source/background.js
+++ source/background.js
@@ -1,1 +1,10 @@
-chrome.action.onClicked.addListener(function (tab) {chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {chrome.tabs.create({ url: "https://translate.google.com/translate?sl=auto&tl=" + navigator.language + "&u=" + tabs[0].url }); }); });
+chrome.action.onClicked.addListener(function (tab) 
+      {chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => 
+            {chrome.tabs.create({ url: 
+                  "https://translate.google.com/translate?sl=auto&tl=" + 
+                  navigator.language + 
+                  "&u=" + 
+                  tabs[0].url 
+            }); 
+      });
+});
